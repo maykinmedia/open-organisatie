fdb183c02ba8ebc682d4ad4c3da1917d71db666f
Tim de Beer <tim@maykinmedia.nl>
2025-07-10 10:16:22 +0200

    :sparkles: [#2] add custom settings schema

---
+++ src/openorganisatie/scim/api/schema.py
@@ -0,0 +1,3 @@
+custom_settings = {
+    "SERVERS": [{"url": "/medewerksers/api/"}],
+}
--- src/openorganisatie/scim/api/urls.py
+++ src/openorganisatie/scim/api/urls.py
@@ -7,6 +7,7 @@ from drf_spectacular.views import (
 )
 from vng_api_common import routers
 
+from .schema import custom_settings
 from .viewsets.medewerker import MedewerkerReadOnlyViewSet
 
 app_name = "scim_api"
@@ -15,22 +16,29 @@ router = routers.DefaultRouter()
 router.register("medewerkers", MedewerkerReadOnlyViewSet)
 
 urlpatterns = [
-    path("", router.APIRootView.as_view(), name="root"),
     path("", include(router.urls)),
-    # OpenAPI schema endpoints
+    path("", router.APIRootView.as_view(), name="root"),
     path(
         "schema/openapi.json",
-        SpectacularJSONAPIView.as_view(urlconf="openorganisatie.scim.api.urls"),
+        SpectacularJSONAPIView.as_view(
+            urlconf="openorganisatie.scim.api.urls",
+            custom_settings=custom_settings,
+        ),
         name="schema-json-medewerkers",
     ),
     path(
         "schema/openapi.yaml",
-        SpectacularYAMLAPIView.as_view(urlconf="openorganisatie.scim.api.urls"),
+        SpectacularYAMLAPIView.as_view(
+            urlconf="openorganisatie.scim.api.urls",
+            custom_settings=custom_settings,
+        ),
         name="schema-yaml-medewerkers",
     ),
     path(
         "schema/",
-        SpectacularRedocView.as_view(url_name="scim_api:schema-yaml-medewerkers"),
+        SpectacularRedocView.as_view(
+            url_name="scim_api:schema-yaml-medewerkers",
+        ),
         name="schema-redoc-medewerkers",
     ),
 ]
