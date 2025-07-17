177047ee9bb16049de61fc87b893e78d1844b59f
Tim de Beer <tim@maykinmedia.nl>
2025-07-17 14:56:13 +0200

    :sparkles: [#16] create endpoint for all teams of a user

---
+++ src/openorganisatie/scim/api/serializers/team.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from openorganisatie.scim.models.team import Team
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    oid = serializers.UUIDField(source="scim_external_id", read_only=True)
+    naam = serializers.CharField(source="name")
+    beschrijving = serializers.CharField(source="description", allow_blank=True)
+
+    class Meta:
+        model = Team
+        fields = ["oid", "naam", "beschrijving"]
