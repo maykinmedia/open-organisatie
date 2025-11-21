6da62f8ee9c281ef8436746b9850f06bbe55b0ee
Tim de Beer <tim@maykinmedia.nl>
2025-11-21 16:08:53 +0100

    :memo: [maykinmedia/open-api-framework#152] update docs

--- src/openorganisatie/organisatie/metrics.py
+++ src/openorganisatie/organisatie/metrics.py
@@ -3,17 +3,17 @@ from opentelemetry import metrics
 meter = metrics.get_meter("openorganisatie.organisatie")
 
 medewerkers_create_counter = meter.create_counter(
-    "openorganisatie.medewerkers.creates",
+    "openorganisatie.medewerker.creates",
     description="Amount of medewerkers created (via the API).",
     unit="1",
 )
 medewerkers_update_counter = meter.create_counter(
-    "openorganisatie.medewerkers.updates",
+    "openorganisatie.medewerker.updates",
     description="Amount of medewerkers updated (via the API).",
     unit="1",
 )
 medewerkers_delete_counter = meter.create_counter(
-    "openorganisatie.medewerkers.deletes",
+    "openorganisatie.medewerker.deletes",
     description="Amount of medewerkers deleted (via the API).",
     unit="1",
 )
